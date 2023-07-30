d2d1a3ad54a6ded471325e91aadfee6edf86cb6b
artem_bakhmatov <ip.bakhmatov@mail.ru>
2023-07-30 21:05:08 +0300

    Contacts Section and animated earth

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+/* eslint-disable react/no-unknown-property */
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
+import CanvasLoader from '../Loader';
 
 const Earth = () => {
-  return (
-    <div>
-      Earch
-    </div>
-  );
+	const earth = useGLTF("./planet/scene.gltf");
+	return (
+		<primitive object={ earth.scene } scale={ 2.5 } position-y={ 0 } rotation-y={ 0 } />
+	);
 };
 
-export default Earth;
+const EarthCanvas = () => {
+	return (
+		<Canvas
+			shadows									// тени
+			frameloop='demand' 						// цикл кадра 'требуемое'
+			gl={{ preserveDrawingBuffer: true }} 	// Сохраненный буфер рисования
+			camera={{
+				fov: 45,
+				near: 0.1,
+				far: 200,
+				position: [-4, 3, 6]
+			}}										// положение камеры
+		>
+			<Suspense fallback={ <CanvasLoader /> }>
+				<OrbitControls 
+					autoRotate 						// автоматический поворот
+					enableZoom={ false }			// масштабирование
+					maxPolarAngle={ Math.PI / 2 }	// макс-ый полярный угол наклона
+					minPolarAngle={ Math.PI / 2 }	// мин-ый полярный угол наклона
+				/> {/* с помощью его мы перемещаем его мышкой */}
+				<Earth />
+			</Suspense>
+		</Canvas>
+	)
+}
+
+export default EarthCanvas;
+
